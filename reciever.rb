541c9a8f2b0aa764a51842eae76d5ebaf0df0109
gbourdon <34389449+gbourdon@users.noreply.github.com>
2018-01-06 22:15:24 -0700

    Set up a basic reciever

---
+++ reciever.rb
@@ -0,0 +1,24 @@
+require_relative 'encoder.rb'
+require 'socket'
+
+class Message
+    attr_reader :type
+    attr_reader :augments
+    attr_reader :message
+
+    def initialize(type, augments, message = nil)
+        augments = augments.parseAugments if augments.is_a? String
+        @type = type
+        @augments = augments
+        @message = message
+    end
+end
+
+class TCPSocket
+    def getMessage
+        type = self.gets.chomp
+        augments = self.gets.chomp.parseAugments
+        message = self.gets.chomp# if augments["Message"] = true
+        Message.new(type, augments,message)
+    end
+end
