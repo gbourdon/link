7cd23bd3e35f6046c2e9461704c1d17acb182519
gbourdon <34389449+gbourdon@users.noreply.github.com>
2018-01-09 21:08:15 -0700

    Added documentation

--- sender.rb
+++ sender.rb
@@ -4,15 +4,15 @@ require 'socket'
 $port = 2000
 
 class Client
-    attr_reader :hostname
-    attr_reader :port
+    attr_reader :hostname # The name of the computer to connect to
+    attr_reader :port # The specific port to connect with
 
-    def initialize(hostname = 'localhost', port = $PORT)
+    def initialize(hostname = 'localhost', port = $PORT) # Gives the client the info it needs to connect to a server
         @hostname = hostname
         @port = port
     end
 
-    def sendMessage(type,augments,message = nil)
+    def sendMessage(type,augments,message = nil) # Sends a message in link format
         s = TCPSocket.open('localhost', 2000)
         augments = augments.encodeAugments if augments.is_a? Hash
         s.puts type
