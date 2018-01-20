f34db5b6621f086591e3580f2efdab5766197a0c
gbourdon <34389449+gbourdon@users.noreply.github.com>
2018-01-19 20:42:58 -0700

    Made the sender more independant

--- sender.rb
+++ sender.rb
@@ -7,13 +7,13 @@ class Client
     attr_reader :hostname # The name of the computer to connect to
     attr_reader :port # The specific port to connect with
 
-    def initialize(hostname = 'localhost', port = $PORT) # Gives the client the info it needs to connect to a server
+    def initialize(hostname = 'localhost', port = $port) # Gives the client the info it needs to connect to a server
         @hostname = hostname
         @port = port
     end
 
     def sendMessage(type,augments,message = nil) # Sends a message in link format
-        s = TCPSocket.open('localhost', 2000)
+        s = TCPSocket.open(@hostname, @port)
         augments = augments.encodeAugments if augments.is_a? Hash
         s.puts type
         s.puts augments
