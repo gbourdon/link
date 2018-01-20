0c7d3ce2ef16d5617025fed297213f706071c060
gbourdon <34389449+gbourdon@users.noreply.github.com>
2018-01-19 20:51:08 -0700

    Added .socket option to directly use a socket to do things

--- sender.rb
+++ sender.rb
@@ -6,17 +6,18 @@ $port = 2000
 class Client
     attr_reader :hostname # The name of the computer to connect to
     attr_reader :port # The specific port to connect with
+    attr_reader :socket
 
     def initialize(hostname = 'localhost', port = $port) # Gives the client the info it needs to connect to a server
         @hostname = hostname
         @port = port
+        @socket = TCPSocket.open(@hostname, @port)
     end
 
     def sendMessage(type,augments,message = nil) # Sends a message in link format
-        s = TCPSocket.open(@hostname, @port)
         augments = augments.encodeAugments if augments.is_a? Hash
-        s.puts type
-        s.puts augments
-        s.puts message
+        @socket.puts type
+        @socket.puts augments
+        @socket.puts message
     end
 end
